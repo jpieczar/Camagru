eac5b1266110ca69924e15680219ac99490fd469
jpieczar <jpieczar@student.wethinkcode.co.za>
2019-10-21 16:28:14 +0200

    Camagru one

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" type= "text/css" href="style.css">
+	</head>
+	<body>
+		<div class="main_top">
+			<h1 class="main_top_logo"><a href="main.html">Camagru</a></h1>
+			<a href="login.php" class="login">Login</a>
+			<a href="register.php" class="register">Register</a>
+		</div>
+		</div>
+			<div class="main_centre_top"></div>
+			<div class="sign">
+		</div>
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Register</title>
+		<link rel="stylesheet" type= "text/css" href="style.css">
+	</head>
+	<body>
+		<div class="main_top">
+			<h1 class="main_top_logo"><a href="main.html">Camagru</a></h1>
+			<a href="login.php" class="login">Login</a>
+			<a href="register.php" class="register">Register</a>
+		</div>
+			<div class="main_centre_top"></div>
+			<div class="main_centre">
+		</div>
+	</body>
+</html>
