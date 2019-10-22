830d627252d0a5512dff37a01d7a5c25f093b421
jpieczar <jpieczar@student.wethinkcode.co.za>
2019-10-22 16:17:48 +0200

    I dunno, lol.

---
+++ camera.js
@@ -0,0 +1,20 @@
+(function() {
+    var video = document.getElementById('video'),
+        vendorURL = window.URL || window.webkitURL;
+
+	navigator.getMedia =	navigator.getUserMedia ||
+							navigator.webkitGetUserMedia ||
+							navigator.mozGetUserMedia ||
+							navigator.oGetUserMedia ||
+							navigator.msGetUserMedia;
+
+    navigator.getMedia({
+        video: true,
+        audio: false
+    }, function(stream) {
+        video.srcObject = stream;
+        video.play();
+    }, function(error) {
+
+    });
+})();
--- register.php
+++ register.php
@@ -8,9 +8,20 @@
 			<h1 class="main_top_logo"><a href="main.html">Camagru</a></h1>
 			<a href="login.php" class="login">Login</a>
 			<a href="register.php" class="register">Register</a>
+			<div class="camera_icon">
+				<img src="img_resources/icons/abstract-user-flat-3.png" alt="user_profile_picture_404"
+				width="auto" height="90px">
+			</div>
 		</div>
-			<div class="main_centre_top"></div>
-			<div class="main_centre">
+		<div class="main_centre_top"></div>
+		<div class="main_centre_small">
+			<form action="register.php" method="post">
+				<input type="text" placeholder="Username">
+				<input type="email" placeholder="Email">
+				<input type="password" placeholder="Password">
+				<input type="password" placeholder="Re-enter Password">
+				<input type="submit" placeholder="Register">
+			</form>
 		</div>
 	</body>
 </html>
