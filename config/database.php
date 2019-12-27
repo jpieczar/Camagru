2c4e8e1001fc47150b48780379978bcd57c72a5c
jpieczar <50695462+jpieczar@users.noreply.github.com>
2019-12-27 13:11:43 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,5 +3,5 @@ $DB_NAME = "appdb";
 $DB_DSN= "mysql:host=localhost";
 $DB_SERVER_DB = "mysql:host=localhost;dbname=".$DB_NAME;
 $DB_USER = "root";
-$DB_PASSWORD = "I_REMOVED_THE_PASSWORD";
+$DB_PASSWORD = "";
 ?>
